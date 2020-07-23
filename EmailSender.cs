c5c952500ec366092759a23b3f51893d9f5fd9e6
Radosław Zamojski <54839731+KedarSki@users.noreply.github.com>
2020-07-23 14:56:09 +0200

    Update EmailSender.cs

--- EmailSender.cs
+++ EmailSender.cs
@@ -28,7 +28,7 @@ namespace WpfApp2
                 mail.Body = body;
 
                 SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential("radoslaw.zamojski@gmail.com", "");
+                SmtpServer.Credentials = new System.Net.NetworkCredential("email@gmail.com", "Password");
                 SmtpServer.EnableSsl = true;
                 
                 SmtpServer.Send(mail);
